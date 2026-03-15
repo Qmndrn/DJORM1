628eec18999f7f66812f2fa8296f1e905e7e5899
Qmndrn <ktoto.ruu@gmail.com>
2026-03-15 11:54:48 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
-
 import django
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
+
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    
+    posts = Passcard.objects.all()
+    active = Passcard.objects.filter(is_active=True)
+
+    print(f'Имя владельца: {posts[0].owner_name}\n активный: {posts[0].passcode}\n дата создания: {posts[0].created_at}\n статус: {posts[0].is_active}')
+
+    print('\nКоличество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    print('Активные пропуска: ', len(active))
+
